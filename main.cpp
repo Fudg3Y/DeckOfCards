f3375048e45dba74d79b333d0e0de104a13e62ae
Jason Smart <bumbummilktoffee@googlemail.com>
2021-01-29 00:43:33 +0000

    Test

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ Card::Card(const string s, const string f) : symbol(s), face(f) {
 }
 
 //Class extends card and holds all 52 cards.
-
+//test
 class Deck : public Card {
 	vector<Card>* deckOfCards;
 	string face[13]{ "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
